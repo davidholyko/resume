46f64b8a5b47fa291d9fd5bced2faddc4dbe2122
David Ko <david.holy.ko@gmail.com>
2019-12-11 17:27:07 -0500

    feat(resume-pdf-upload): add link directly to pdf file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,11 @@
 import React from 'react'
+import pdf from '../resume_pdfs/2019_12_13_David_Ko_Software_Engineer.pdf'
 // import pdf from 'resume_pdfs/David_Ko_Software_Engineer.pdf'
 
 const Header = () => (
   <div id="header">
     <button className="btn btn-success">
-      <a
-        href="https://drive.google.com/open?id=1eYtaE4WahMtCUD8F0wiMGIl2ZUo6uTdt"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href={pdf} target="_blank" rel="noopener noreferrer">
         <h4 className="btn-link-text">Download PDF</h4>
       </a>
     </button>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import { BrowserRouter } from 'react-router-dom'
 import './css/index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Application = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
+ReactDOM.render(<Application />, document.getElementById('root'))
