0f6f993d7379cf84309a2b14b6a19e70b566a6ac
David Ko <david.holy.ko@gmail.com>
2019-12-11 14:56:11 -0500

    feat(print): add react-to-print

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 import InfoColumn from './columns/InfoColumn'
 import ContentColumn from './columns/ContentColumn'
+import ReactToPrint from 'react-to-print'
 
 class Resume extends Component {
   render () {
     return (
       <div id="resume">
         <InfoColumn />
-        <ContentColumn/>
+        <ContentColumn />
       </div>
     )
   }
 }
 
-export default Resume
+class Print extends Component {
+  render () {
+    return (
+      <React.Fragment>
+        <ReactToPrint
+          trigger={() => <a href="#">Print this out!</a>}
+          content={() => this.componentRef}
+        />
+        <Resume ref={el => (this.componentRef = el)} />
+      </React.Fragment>
+    )
+  }
+}
+
+export default Print
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import { HashRouter } from 'react-router-dom'
 import './css/index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const appJsx = (
-  <HashRouter>
-    <App />
-  </HashRouter>
-)
-
-ReactDOM.render(appJsx, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
