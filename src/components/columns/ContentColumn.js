c51ba33eb2a7ea2047d48d10635bf61dfe030f7e
David Ko <david.holy.ko@gmail.com>
2019-12-11 12:39:16 -0500

    feat(work): add software engineering experience

--- src/components/columns/ContentColumn.js
+++ src/components/columns/ContentColumn.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import ContentSummary from './content/ContentSummary'
+// import ContentSummary from './content/ContentSummary'
 import ContentProjects from './content/ContentProjects'
 import ContentWorkHistory from './content/ContentWorkHistory'
 import ContentEducation from './content/ContentEducation'
+import ContentSoftwareExperience from './content/ContentSoftwareExperience'
 
 const ContentColumn = () => (
   <div id="content-column">
-    <ContentSummary />
+    {/* <ContentSummary /> */}
+    <ContentSoftwareExperience />
     <ContentProjects />
     <ContentEducation />
     <ContentWorkHistory />
---
+++ src/components/columns/content/ContentSoftwareExperienceItem.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const ContentSoftwareExperience = ({ work }) => (
+  <div className="content-work-item">
+    <div className="d-flex justify-content-between">
+      <div>
+        <span className="font-weight-bold">{work.company}</span>
+        <span> | </span>
+        <span className="font-italic">{work.location}</span>
+      </div>
+      <span className="ml-auto">{work.date}</span>
+    </div>
+    <div className="d-flex flex-wrap mb-1">
+      {work.tags.map((tag, index) => (
+        <span
+          key={index}
+          className="bg-dark text-light rounded mr-2 px-2 my-1 skill"
+        >
+          {tag}
+        </span>
+      ))}
+    </div>
+    <ul>
+      {work.descriptions.map((description, index) => (
+        <li key={index}>{description}</li>
+      ))}
+    </ul>
+  </div>
+)
+
+export default ContentSoftwareExperience
